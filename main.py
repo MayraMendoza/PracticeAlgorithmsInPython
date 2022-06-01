7ebf21afbf73cbbad4752f952ccd0710ab683fb8
MayraMendoza <mayra601m@gmail.com>
2022-05-31 20:26:19 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# #1
+# Return the First Element in an Array (getfirstelement.js)
+# https://edabit.com/challenge/QaApgtePE6QrCZ64o
+
+# def get_first_value(number_list):
+#     return number_list[0]
+#
+# number = get_first_value([-52320, 0, 50])
+# #get_first_value([75675, 5, 100])
+# print(number)
+
+#*****************************************************************
